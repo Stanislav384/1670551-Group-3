8ee4e5a1714a087505d701c0ce7967c88f1a8933
Nazar44455 <nazar.rudenko2020@gmail.com>
2025-09-06 22:58:53 +0200

    7

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ menu_txt.addEventListener('click', function(){
         arrowImg.classList.remove('rotated');
     }
 })
+
+
+
+let left_arrow = document.querySelector('#left_arrow');
+let dot1 = document.querySelector('#dot1');
+let dot2 = document.querySelector('#dot2');
+let dot3 = document.querySelector('#dot3');
+let dot4 = document.querySelector('#dot4');
+let dot5 = document.querySelector('#dot5');
+let dot6 = document.querySelector('#dot6');
+let right_arrow = document.querySelector('#right_arrow');
+
+let s_img1 = document.querySelector('#s_img1');
+let s_img2 = document.querySelector('#s_img2');
+let s_img3 = document.querySelector('#s_img3');
+let s_img4 = document.querySelector('#s_img4');
+let s_img5 = document.querySelector('#s_img5');
+
